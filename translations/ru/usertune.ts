8727ec310cbe64fba8a20aff9da19584d65160b1
Andrey Karepin <egd.free@gmail.com>
2012-03-31 19:05:34 +0400

    обновил файлы с переводами, добавил пару слов на русском

--- translations/ru/usertune.ts
+++ translations/ru/usertune.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" sourcelanguage="en">
+<TS version="2.0" language="ru_RU" sourcelanguage="en">
 <context>
     <name>UserTuneHandler</name>
     <message>
@@ -10,6 +10,42 @@
     <message>
         <source>Allows hadle user tunes</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>User tune</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>User tune notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>When reminding of contact playing music</source>
+        <translation type="unfinished">При получении извещения от контакта о проигрываемой музыке</translation>
+    </message>
+    <message>
+        <source>User Tune</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show music icon in roster</source>
+        <translation>Показывать значок в ростере</translation>
+    </message>
+    <message>
+        <source>Tag format:</source>
+        <translation>Расстановка тегов:</translation>
+    </message>
+    <message>
+        <source>Player name:</source>
+        <translation>Плеер:</translation>
+    </message>
+    <message>
+        <source>User Tune Notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Listen:</source>
+        <translation>Слушает:</translation>
     </message>
 </context>
 </TS>
--- translations/uk/usertune.ts
+++ translations/uk/usertune.ts
@@ -11,5 +11,41 @@
         <source>Allows hadle user tunes</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>User tune</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>User tune notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>When reminding of contact playing music</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>User Tune</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show music icon in roster</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tag format:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Player name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>User Tune Notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Listen:</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 </TS>
