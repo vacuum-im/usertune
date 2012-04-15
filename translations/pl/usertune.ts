a11b5b5a47d9c993050dc056a1419ff8d1d7736f
Andrey Karepin <egd.free@gmail.com>
2012-04-15 09:17:40 +0400

    обновлены файды переводов после добавления формы

--- translations/pl/usertune.ts
+++ translations/pl/usertune.ts
@@ -1,50 +1,79 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" sourcelanguage="en">
+<TS version="2.0">
 <context>
     <name>UserTuneHandler</name>
     <message>
+        <location filename="../../usertune.cpp" line="43"/>
         <source>User Tune Handler</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../usertune.cpp" line="44"/>
         <source>Allows hadle user tunes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../usertune.cpp" line="125"/>
         <source>User tune</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../usertune.cpp" line="130"/>
         <source>User tune notification</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../usertune.cpp" line="139"/>
         <source>When reminding of contact playing music</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../usertune.cpp" line="174"/>
         <source>User Tune</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Show music icon in roster</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Tag format:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Player name:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
+        <location filename="../../usertune.cpp" line="230"/>
         <source>User Tune Notification</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../usertune.cpp" line="469"/>
         <source>Listen:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UserTuneOptions</name>
+    <message>
+        <location filename="../../usertuneoptions.ui" line="14"/>
+        <source>User Tune</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../usertuneoptions.ui" line="26"/>
+        <source>Show music icon in roster</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../usertuneoptions.ui" line="33"/>
+        <source>Tag format:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../usertuneoptions.ui" line="59"/>
+        <source>Player name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../usertuneoptions.ui" line="82"/>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../usertuneoptions.cpp" line="33"/>
+        <source>Not selected</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- translations/ru/usertune.ts
+++ translations/ru/usertune.ts
@@ -1,51 +1,80 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU" sourcelanguage="en">
+<TS version="2.0" language="ru_RU" sourcelanguage="en_GB">
 <context>
     <name>UserTuneHandler</name>
     <message>
+        <location filename="../../usertune.cpp" line="43"/>
         <source>User Tune Handler</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../usertune.cpp" line="44"/>
         <source>Allows hadle user tunes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../usertune.cpp" line="125"/>
         <source>User tune</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../usertune.cpp" line="130"/>
         <source>User tune notification</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../usertune.cpp" line="139"/>
         <source>When reminding of contact playing music</source>
         <translation type="unfinished">При получении извещения от контакта о проигрываемой музыке</translation>
     </message>
     <message>
+        <location filename="../../usertune.cpp" line="174"/>
         <source>User Tune</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Show music icon in roster</source>
-        <translation>Показывать значок в ростере</translation>
+        <location filename="../../usertune.cpp" line="230"/>
+        <source>User Tune Notification</source>
+        <translation type="unfinished">Оповещение User Tune</translation>
     </message>
     <message>
+        <location filename="../../usertune.cpp" line="469"/>
+        <source>Listen:</source>
+        <translation>Слушает:</translation>
+    </message>
+</context>
+<context>
+    <name>UserTuneOptions</name>
+    <message>
+        <location filename="../../usertuneoptions.ui" line="14"/>
+        <source>User Tune</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../usertuneoptions.ui" line="26"/>
+        <source>Show music icon in roster</source>
+        <translation>Показывать значок ноты в ростере</translation>
+    </message>
+    <message>
+        <location filename="../../usertuneoptions.ui" line="33"/>
         <source>Tag format:</source>
         <translation>Расстановка тегов:</translation>
     </message>
     <message>
+        <location filename="../../usertuneoptions.ui" line="59"/>
         <source>Player name:</source>
         <translation>Плеер:</translation>
     </message>
     <message>
-        <source>User Tune Notification</source>
-        <translation type="unfinished"></translation>
+        <location filename="../../usertuneoptions.ui" line="82"/>
+        <source>Refresh</source>
+        <translation>Обновить</translation>
     </message>
     <message>
-        <source>Listen:</source>
-        <translation>Слушает:</translation>
+        <location filename="../../usertuneoptions.cpp" line="33"/>
+        <source>Not selected</source>
+        <translation>Не выбран</translation>
     </message>
 </context>
 </TS>
