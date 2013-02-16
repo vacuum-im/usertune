2129cfd70f384bd1a341dea044c266d33dadb21c
Alexey Ivanov <alexey.ivanes@gmail.com>
2013-02-17 03:28:59 +0400

    Переводы из default

--- translations/pl/usertune.ts
+++ translations/pl/usertune.ts
@@ -1,89 +1,93 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="pl" sourcelanguage="en">
 <context>
     <name>UserTuneHandler</name>
     <message>
-        <location filename="../../usertune.cpp" line="51"/>
         <source>User Tune Handler</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertune.cpp" line="52"/>
         <source>Allows hadle user tunes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertune.cpp" line="143"/>
         <source>User tune</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertune.cpp" line="148"/>
         <source>User tune notification</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertune.cpp" line="157"/>
         <source>When reminding of contact playing music</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertune.cpp" line="191"/>
         <source>User Tune</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertune.cpp" line="254"/>
-        <source>User Tune Notification</source>
+        <source>Listen:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertune.cpp" line="591"/>
-        <source>Listen:</source>
+        <source>Show music icon in roster</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tag format:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Player name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>listening to</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Music info</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>UserTuneOptions</name>
     <message>
-        <location filename="../../usertuneoptions.ui" line="14"/>
         <source>User Tune</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertuneoptions.ui" line="20"/>
         <source>Show format:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertuneoptions.ui" line="49"/>
         <source>Show music icon in roster</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertuneoptions.ui" line="30"/>
         <source>Player name:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertuneoptions.ui" line="69"/>
         <source>Refresh</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertuneoptions.ui" line="76"/>
         <source>MPRIS Version:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertuneoptions.ui" line="89"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;%A - artis&lt;br/&gt;%L - length&lt;br/&gt;%R - rating&lt;br/&gt;%S - source&lt;br/&gt;%T - title&lt;br/&gt;%N - track&lt;br/&gt;%U - URL&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;%A - artis&lt;br/&gt;%L - length&lt;br/&gt;%R - rating&lt;br/&gt;%S - source&lt;br/&gt;%T - title&lt;br/&gt;%N - track&lt;br/&gt;%U - URL&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Allow to send information about playing music</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../usertuneoptions.cpp" line="17"/>
-        <source>Not selected</source>
+        <source>Never send a URL tag</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
