7f25261c7ec889f85e37f6b6cec337776582d2cd
Andrey Karepin <egdfree@opensuse.org>
2017-01-15 23:32:47 +0300

    build with Qt5

--- imetadatafetcher.cpp
+++ imetadatafetcher.cpp
@@ -3,61 +3,49 @@
 
 QStringList getPlayersList(const int &ver)
 {
-    QStringList ret_list;
-#ifdef Q_WS_X11
+	QStringList ret_list;
 	QStringList services = QDBusConnection::sessionBus().interface()->registeredServiceNames().value().filter(ORG_MPRIS_1);
-#elif Q_WS_WIN
 
-#endif
-
-    switch (ver)
-    {
-#ifdef  Q_WS_X11
-    case FetcherVer::mprisV1:
-        foreach (QString service, services)
-        {
+	switch (ver)
+	{
+	case FetcherVer::mprisV1:
+		foreach (QString service, services)
+		{
 			if (!service.startsWith(ORG_MPRIS_2))
 				ret_list << service.replace(ORG_MPRIS_1,"");
-        }
-        break;
-     case FetcherVer::mprisV2:
-        foreach (QString service, services)
-        {
+		}
+		break;
+	 case FetcherVer::mprisV2:
+		foreach (QString service, services)
+		{
 			if (service.startsWith(ORG_MPRIS_2))
 				ret_list << service.replace(ORG_MPRIS_2,"");
-        }
-        break;
-#elif Q_WS_WIN
+		}
+		break;
+	default:
+		break;
+	}
 
-#endif
-    default:
-        break;
-    }
-
-    return ret_list;
+	return ret_list;
 }
 
 IMetaDataFetcher::IMetaDataFetcher(QObject *parent) :
-    QObject(parent)
+	QObject(parent)
 {
-#ifdef  Q_WS_X11
-    QDBusConnection::sessionBus().connect("org.freedesktop.DBus",
-                                          "/org/freedesktop/DBus",
-                                          "org.freedesktop.DBus",
-                                          "NameOwnerChanged",
-                                          this,
-                                          SLOT(onPlayersExistenceChanged(QString, QString, QString)));
-#endif
+	QDBusConnection::sessionBus().connect("org.freedesktop.DBus",
+										  "/org/freedesktop/DBus",
+										  "org.freedesktop.DBus",
+										  "NameOwnerChanged",
+										  this,
+										  SLOT(onPlayersExistenceChanged(QString, QString, QString)));
 }
 
 IMetaDataFetcher::~IMetaDataFetcher()
 {
-#ifdef  Q_WS_X11
-    QDBusConnection::sessionBus().disconnect("org.freedesktop.DBus",
-                                           "/org/freedesktop/DBus",
-                                           "org.freedesktop.DBus",
-                                           "NameOwnerChanged",
-                                           this,
-                                           SLOT(onPlayersExistenceChanged(QString, QString, QString)));
-#endif
+	QDBusConnection::sessionBus().disconnect("org.freedesktop.DBus",
+											 "/org/freedesktop/DBus",
+											 "org.freedesktop.DBus",
+											 "NameOwnerChanged",
+											 this,
+											 SLOT(onPlayersExistenceChanged(QString, QString, QString)));
 }
